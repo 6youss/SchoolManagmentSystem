77e9c7cda458912a2895705be9e2a24d822d886f
islamblmrbt <belmerabetislam@gmail.com>
2020-04-09 17:08:34 +0100

    user info

--- app/api/UserInfoController.php
+++ app/api/UserInfoController.php
@@ -12,7 +12,7 @@ if(isset($_GET["view"]) ){
     $id="";
     if(isset($_GET["id"])){
     $id = $_GET["id"];
-}*/
+}
 
 $view = "";
 if(isset($_GET["view"]) ){
@@ -21,10 +21,10 @@ if(isset($_GET["view"]) ){
     $userName="";$password="";
     if(isset($_GET["userName"]) ){
     $userName = $_GET["userName"];
-    }
+    }*/
 
     
-/*$json = file_get_contents('php://input');
+$json = file_get_contents('php://input');
  
 // decoding the received JSON and store into $obj variable.
 $obj = json_decode($json,true);
@@ -32,7 +32,7 @@ $obj = json_decode($json,true);
 // name store into $name.
 $view = $obj['view'];
 $userName=$obj['userName'];
-$password=$obj['password'];*/
+$password=$obj['password'];
     switch($get){
 
         /*case "all":

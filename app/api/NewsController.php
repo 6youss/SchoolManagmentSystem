d93d0e9cb5f6663bdbd9615ddcfe94d0c2ae43bf
islamblmrbt <belmerabetislam@gmail.com>
2020-04-10 16:56:27 +0100

    news controller

--- app/api/NewsController.php
+++ app/api/NewsController.php
@@ -3,11 +3,13 @@ require_once("NewsRestHandler.php");
 require_once("News.php");		
 
 
-{///////view
-	$view = "";
-if(isset($_GET["view"]) ){
-	$view = $_GET["view"];
-    }}
+$json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+	$view = $obj['view'];
     
     switch($view){
 

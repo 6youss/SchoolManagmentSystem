2d98017ca324c5543eabe33fd7a1087e2a7c3334
islamblmrbt <belmerabetislam@gmail.com>
2020-04-12 19:45:52 +0100

    pages controller view

--- app/api/StaticPagesController.php
+++ app/api/StaticPagesController.php
@@ -3,11 +3,13 @@ require_once("StaticPagesRestHandler.php");
 require_once("StaticPages.php");		
 
 
-{///////view
-	$view = "";
-if(isset($_GET["view"]) ){
-	$view = $_GET["view"];
-    }}
+$json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+	$view = $obj['view'];
     
     switch($view){
 

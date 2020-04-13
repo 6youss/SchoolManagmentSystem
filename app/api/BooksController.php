e6ce4ac6405ed6d46e776abb5eda78c1d0ad1176
islamblmrbt <belmerabetislam@gmail.com>
2020-04-13 21:27:20 +0100

    books controller

--- app/api/BooksController.php
+++ app/api/BooksController.php
@@ -2,12 +2,20 @@
 require_once("BooksRestHandler.php");
 require_once("Books.php");		
 
+$json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+	$view = $obj['view'];
 
+/*
 {///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
-    }}
+    }}*/
 
    { $title = "";
 if(isset($_GET["title"]) ){

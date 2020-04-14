426fa274663019c75979739ca0818b9cdfd33561
islamblmrbt <belmerabetislam@gmail.com>
2020-04-14 11:59:05 +0100

    polls controller

--- app/api/PollsController.php
+++ app/api/PollsController.php
@@ -2,12 +2,20 @@
 require_once("PollsRestHandler.php");
 require_once("Polls.php");		
 
-
+/*
 {///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
-    }}
+    }}*/
+
+    $json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+	$view = $obj['view'];
     
     switch($view){
 

84d3b6f7888badd838a1047672411504e55e9f1c
islamblmrbt <belmerabetislam@gmail.com>
2020-04-14 10:47:57 +0100

    messages Controller

--- app/api/MessagesController.php
+++ app/api/MessagesController.php
@@ -3,33 +3,42 @@ require_once("MessagesRestHandler.php");
 require_once("Messages.php");		
 
 
-{///////view
+/*{///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
     }}
 
    { $id = "";
-if(isset($_GET["id"]) ){
-	$id = $_GET["id"];
-    }}
+if(isset($_GET["userId"]) ){
+	$userId = $_GET["userId"];
+    }}*/
+
+    $json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+    $view = $obj['view'];
+    $userId= $obj['userId'];
     
     switch($view){
 
         case "all":
             // to handle REST Url /mobile/list/
             $messagesRestHandler = new MessagesRestHandler();
-            $messagesRestHandler->getAllMessages($id);
+            $messagesRestHandler->getAllMessages($userId);
             break;
 
         case "sent":
             // to handle REST Url /mobile/list/
             $messagesRestHandler = new MessagesRestHandler();
-            $messagesRestHandler->getSentMessages($id);
+            $messagesRestHandler->getSentMessages($userId);
             break;
         case "recieved":
             $messagesRestHandler = new MessagesRestHandler();
-            $messagesRestHandler->getReceivedMessages($id);
+            $messagesRestHandler->getReceivedMessages($userId);
         break;
 
         }

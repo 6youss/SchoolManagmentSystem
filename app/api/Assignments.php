e5b5feffa88cd242f2dc0670792a768b4772df8a
islamblmrbt <belmerabetislam@gmail.com>
2020-04-16 20:44:36 +0100

    teacher class assignments

--- app/api/Assignments.php
+++ app/api/Assignments.php
@@ -56,10 +56,10 @@ Class Assignments {
 	}
 
 	public function getTeacherClassAssignments($uid,$id){
-		$query = "SELECT id FROM assignments where teacherId=".$uid;
+		/*$query = "SELECT id FROM assignments where teacherId=".$uid;
 		$dbcontroller = new DBController();
 		$ids = $dbcontroller->executeSelectQuery($query);
-				$query = "SELECT classId FROM assignments where teacherId=".$uid;
+		$query = "SELECT classId FROM assignments where teacherId=".$uid;
 		$classids = $dbcontroller->executeSelectQuery($query);
 		$str=substr($classids[0]["classId"],1,-1);
 		$classids=explode(",",$str);
@@ -83,6 +83,31 @@ Class Assignments {
 			}
 			}
 			}
+		$this->assignments = $dbcontroller->executeSelectQuery($query);
+		return $this->assignments;*/
+		$query = "SELECT id FROM assignments where teacherId=".$uid;
+		$dbcontroller = new DBController();
+		$ids = $dbcontroller->executeSelectQuery($query);
+		$query = "SELECT classId FROM assignments where teacherId=".$uid;
+		$classids = $dbcontroller->executeSelectQuery($query);
+        $fids=array();
+		for($i=0;$i<sizeof($ids);$i++){
+        $str=substr($classids[$i]["classId"],1,-1);
+		$cida=explode(",",$str);
+        for($j=0;$j<sizeof($cida);$j++){
+        if(substr($cida[$j],1,-1)==$id){
+			array_push($fids,$ids[$i]["id"]);
+		}
+		}
+		$query = "SELECT * FROM assignments where id=";
+		for($k=0;$k<sizeof($fids);$k++){
+		if($k==(sizeof($fids)-1)){
+			$query=$query.$fids[$k];
+		}else{
+			$query=$query.$fids[$k]." or id=";
+		}
+        }
+		}
 		$this->assignments = $dbcontroller->executeSelectQuery($query);
 		return $this->assignments;
 	}	

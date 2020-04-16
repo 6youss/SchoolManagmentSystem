636ec02f39a2b04545b37d96d9e60609e85d7605
islamblmrbt <belmerabetislam@gmail.com>
2020-04-16 20:32:22 +0100

    assignments filter teacher controller

--- app/api/AssignmentsFilterTeacherController.php
+++ app/api/AssignmentsFilterTeacherController.php
@@ -2,8 +2,17 @@
 require_once("AssignmentsRestHandler.php");
 require_once("Assignments.php");		
 
-
-{///////view
+$json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+    $view = $obj['view'];
+    $id= $obj['id'];
+    $uid=$obj['uid'];
+    
+/*{///////view
 	$view = "";$id="";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
@@ -18,7 +27,7 @@ if(isset($_GET["id"])){
 	$uid="";
     if(isset($_GET["uid"])){
         $uid = $_GET["uid"];
-        }}
+        }}*/
     switch($view){
 
        

83ee2adb361b9ebe2931cb55fdcda0f5e42e1087
islamblmrbt <belmerabetislam@gmail.com>
2020-04-16 11:42:25 +0100

    classes schedule done

--- app/api/ClassesScheduleController.php
+++ app/api/ClassesScheduleController.php
@@ -2,16 +2,16 @@
 require_once("ClassesScheduleRestHandler.php");
 require_once("ClassesSchedule.php");		
 
-/*$json = file_get_contents('php://input');
+$json = file_get_contents('php://input');
  
 	 // decoding the received JSON and store into $obj variable.
 	 $obj = json_decode($json,true);
 	 
 	 // name store into $name.
     $view = $obj['view'];
-    $classId=$obj['classId'];*/
+    $classId=$obj['classId'];
 
-{///////view
+/*{///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
@@ -21,14 +21,14 @@ if(isset($_GET["view"]) ){
 	$id = "";
 if(isset($_GET["id"]) ){
 	$id = $_GET["id"];
-    }}
+    }}*/
 
     switch($view){
 
         case "get":
             // to handle REST Url /mobile/list/
             $classesscheduleRestHandler = new ClassesScheduleRestHandler();
-        $classesscheduleRestHandler->getClassSchedule(/*$classId*/$id);
+            $classesscheduleRestHandler->getClassSchedule($classId);
             break;
 
 

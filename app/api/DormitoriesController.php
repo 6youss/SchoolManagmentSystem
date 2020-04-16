d7bc273e6980ec177fb8d56200a6e903a05328ee
islamblmrbt <belmerabetislam@gmail.com>
2020-04-16 12:58:43 +0100

    dormitories controller

--- app/api/DormitoriesController.php
+++ app/api/DormitoriesController.php
@@ -2,8 +2,16 @@
 require_once("DormitoriesRestHandler.php");
 require_once("Dormitories.php");		
 
+$json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+    $view = $obj['view'];
+    $dormitoryId=$obj['dormitoryId'];
 
-{///////view
+/*{///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
@@ -13,14 +21,14 @@ if(isset($_GET["view"]) ){
 	$id = "";
 if(isset($_GET["id"]) ){
 	$id = $_GET["id"];
-    }}
+    }}*/
 
     switch($view){
 
         case "get":
             // to handle REST Url /mobile/list/
             $DormitoriesRestHandler = new DormitoriesRestHandler();
-            $DormitoriesRestHandler->getDormitorie($id);
+            $DormitoriesRestHandler->getDormitorie($dormitoryId);
             break;
 
 

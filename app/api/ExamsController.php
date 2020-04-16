67d3689025e27e2fc3e582163d77e9230dead184
islamblmrbt <belmerabetislam@gmail.com>
2020-04-16 12:17:03 +0100

    exams view

--- app/api/ExamsController.php
+++ app/api/ExamsController.php
@@ -3,13 +3,19 @@ require_once("ExamsRestHandler.php");
 require_once("Exams.php");		
 
 
-{///////view
+/*{///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
-    }}
+    }}*/
     
-
+    $json = file_get_contents('php://input');
+ 
+    // decoding the received JSON and store into $obj variable.
+    $obj = json_decode($json,true);
+    
+    // name store into $name.
+   $view = $obj['view'];
 
     switch($view){
 

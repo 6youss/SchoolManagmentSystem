f0204bef952c96923cc58c75c3b5f38222249d53
islamblmrbt <belmerabetislam@gmail.com>
2020-04-27 17:27:50 +0100

    student info view

--- app/api/StudentInfoController.php
+++ app/api/StudentInfoController.php
@@ -2,7 +2,7 @@
 require_once("UserInfoRestHandler.php");
 require_once("UserInfo.php");		
 
-/*
+
 {
 	$view = "";
 if(isset($_GET["view"]) ){
@@ -12,16 +12,16 @@ if(isset($_GET["view"]) ){
         $studentId = "";
     if(isset($_GET["studentId"]) ){
         $studentId = $_GET["studentId"];
-        }}*/
+        }}
     
-$json = file_get_contents('php://input');
+/*$json = file_get_contents('php://input');
  
 // decoding the received JSON and store into $obj variable.
 $obj = json_decode($json,true);
 
 // name store into $name.
 $view = $obj['view'];
-$studentId=$obj['studentId'];
+$studentId=$obj['studentId'];*/
     switch($view){
 
   

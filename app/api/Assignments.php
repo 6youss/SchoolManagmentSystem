6d54eee2d9fc69fd2a18fbd703481b70010fe431
islamblmrbt <belmerabetislam@gmail.com>
2020-05-01 16:35:35 +0100

    teacher assignments test

--- app/api/Assignments.php
+++ app/api/Assignments.php
@@ -124,8 +124,9 @@ Class Assignments {
 	}
 
 	public function getTeacherAssignments($id){
-		$query = "SELECT a.AssignTitle,a.AssignDescription,a.AssignFile,a.AssignDeadLine,s.subjectTitle 
-		FROM assignments a,subject s where a.subjectId=s.id and a.teacherId= ".$id;
+		$query = "SELECT * FROM assignments";
+		/*$query = "SELECT a.AssignTitle,a.AssignDescription,a.AssignFile,a.AssignDeadLine,s.subjectTitle 
+		FROM assignments a,subject s where a.subjectId=s.id and a.teacherId=".$id;*/
 		$dbcontroller = new DBController();
 		$this->assignments = $dbcontroller->executeSelectQuery($query);
 		return $this->assignments;

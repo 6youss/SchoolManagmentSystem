b42b1fb818f16ac67274ea3ed07ae74517159cc6
islamblmrbt <belmerabetislam@gmail.com>
2020-05-02 13:58:09 +0100

    school infos

--- app/api/SchoolInfoController.php
+++ app/api/SchoolInfoController.php
@@ -3,15 +3,23 @@ require_once("SchoolInfoRestHandler.php");
 require_once("SchoolInfo.php");		
 
 
-{///////view
+/*{///////view
 	$view = "";
 if(isset($_GET["view"]) ){
 	$view = $_GET["view"];
-    }}
+    }}*/
+
+    $json = file_get_contents('php://input');
+ 
+	 // decoding the received JSON and store into $obj variable.
+	 $obj = json_decode($json,true);
+	 
+	 // name store into $name.
+	$view = $obj['view'];
     
     switch($view){
 
-        case "all":
+        case "get":
             // to handle REST Url /mobile/list/
             $schoolinfoRestHandler = new SchoolInfoRestHandler();
             $schoolinfoRestHandler->getAllSchoolInfo();

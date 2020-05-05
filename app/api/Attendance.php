d85614307b1dcc315fd8934f99a6bce017d0f875
islamblmrbt <belmerabetislam@gmail.com>
2020-05-05 12:05:30 +0100

    student all subjects attendance

--- app/api/Attendance.php
+++ app/api/Attendance.php
@@ -19,7 +19,7 @@ Class Attendance {
 	public function getSubjectsAttendance($classId,$date,$value){
 		$query = "SELECT a.date,a.status,cl.className,su.subjectTitle,st.fullName,st.studentRollId,st.photo 
 		FROM attendance a,classes cl,subject su,users st
-		where a.classId=".$classId." and cl.id=".$classId." and a.studentId=".$studentId." and st.id=".$studentId
+		where a.classId=".$classId." and cl.id=".$classId." and su.id=a.subjectId and a.studentId=".$studentId." and st.id=".$studentId
 		." and a.date='".$date."'";
 		$dbcontroller = new DBController();
 		$this->attendance = $dbcontroller->executeSelectQuery($query);

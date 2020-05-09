5f10d4cced0fbd5cd8e6a4a9c432ad4ed1ad6d18
islamblmrbt <belmerabetislam@gmail.com>
2020-05-09 14:23:53 +0100

    student class online exams

--- app/api/OnlineExams.php
+++ app/api/OnlineExams.php
@@ -10,11 +10,34 @@ Class OnlineExams {
 		you should hookup the DAO here
 	*/
 	public function getOnlineExams($classId,$studentId,$date){
-		$query = "SELECT * FROM onlineexams where id not in (select examId from onlineexamsgrades where studentId=".$studentId.")
-         order by id desc";
         $dbcontroller = new DBController();
+		$query = "SELECT id FROM onlineexams where id not in (select examId from onlineexamsgrades where studentId=".$studentId.")";
+        $ids=$dbcontroller->executeSelectQuery($query);
+        $query = "SELECT examClass FROM onlineexams where id not in (select examId from onlineexamsgrades where studentId=".$studentId.")";
+        $classes=$dbcontroller->executeSelectQuery($query);
+        $fids=array();
+        for($i=0;$i<sizeof($ids);$i++){
+            $str=substr($classes[$i]["examClass"],1,-1);
+            $cida=explode(",",$str);
+            for($j=0;$j<sizeof($cida);$j++){
+            if(substr($cida[$j],1,-1)==$classId){
+                array_push($fids,$ids[$i]["id"]);
+            }
+            }
+            $query = "SELECT oe.id,oe.examTitle,oe.examDescription,oe.examQuestion,oe.examDate,oe.examEndDate,
+            t.id,t.userName,t.email.t.photo,
+            s.id,s.subjectTitle
+            FROM onlineexams oe,users t,subject s where oe.examTeacher=t.id and oe.examSubject=s.id and (oe.id=";
+            for($k=0;$k<sizeof($fids);$k++){
+            if($k==(sizeof($fids)-1)){
+                $query=$query.$fids[$k].")";
+            }else{
+                $query=$query.$fids[$k]." or oe.id=";
+            }
+            }
+            }
         $exams=array();
-        $exams=$dbcontroller->executeSelectQuery($query);
+        
         for($i=0;$i<sizeof($exams);$i++){
             if(intval($exams[$i]['ExamEndDate']) <= intval($date) == 1){
                 $exams[$i]['ExamEndDate']=date('m/d/Y',$exams[$i]['ExamEndDate']);

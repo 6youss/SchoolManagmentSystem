22741c2fd327fd607bfc91fa4870ec2a95648f08
islamblmrbt <belmerabetislam@gmail.com>
2020-05-15 14:14:39 +0100

    fix

--- app/api/OnlineExamsController2.php
+++ app/api/OnlineExamsController2.php
@@ -4,19 +4,19 @@ require_once("OnlineExams.php");
 
 
 
-	$view = "";
+	/*$view = "";
 if(isset($_POST["view"]) ){
 	$view = $_POST["view"];
-    }
+    }*/
     
-    /*$json = file_POST_contents('php://input');
+    $json = file_POST_contents('php://input');
  
     // decoding the received JSON and store into $obj variable.
     $obj = json_decode($json,true);
     
     // name store into $name.
    $view = $obj['view'];
-   $classId = $obj['classId'];
+   /*$classId = $obj['classId'];
    $studentId = $obj['studentId'];
    $subjectId = $obj['subjectId'];
    $date = $obj['date'];*/

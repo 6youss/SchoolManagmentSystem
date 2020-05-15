3cd61361e2b58a54d0a7694865d03dbd558bf9f5
islamblmrbt <belmerabetislam@gmail.com>
2020-05-15 15:07:58 +0100

    post test

--- app/api/OnlineExamsController2.php
+++ app/api/OnlineExamsController2.php
@@ -1,9 +1,9 @@
 <?php
 require_once("OnlineExamsRestHandler.php");
 require_once("OnlineExams.php");	
-/*
-//$view = $_POST["view"];
 
+$view = $_POST["view"];
+/*
 $view = "";
 if(isset($_POST["view"]))
     {$view = $_POST["view"];}
@@ -29,14 +29,14 @@ if(isset($_POST["view"]) ){
 	$view = $_POST["view"];
     }*/
     
-    $json = file_get_contents('php://input');
+   /* $json = file_get_contents('php://input');
  
     // decoding the received JSON and store into $obj variable.
     $obj = json_decode($json,true);
     
     // name store into $name.
    $view = $obj['view'];
-   /*$classId = $obj['classId'];
+   $classId = $obj['classId'];
    $studentId = $obj['studentId'];
    $subjectId = $obj['subjectId'];
    $date = $obj['date'];*/
@@ -46,9 +46,9 @@ if(isset($_POST["view"]) ){
         case "POSt":
             // to handle REST Url /mobile/list/
             $examsRestHandler = new ExamsRestHandler();
-           // $examsRestHandler->POStSubjectOnlineExams($classId,$studentId,$subjectId,$date);
-          // $examsRestHandler->POStSubjectOnlineExams($_POST["classId"],$_POST["studentId"],$_POST["subjectId"],$_POST["date"]);
-           $examsRestHandler->POStSubjectOnlineExams($obj['classId'],$obj['studentId'],$obj['subjectId'],$obj['date']);
+           // $examsRestHandler->getSubjectOnlineExams($classId,$studentId,$subjectId,$date);
+           $examsRestHandler->getSubjectOnlineExams($_POST["classId"],$_POST["studentId"],$_POST["subjectId"],$_POST["date"]);
+           //$examsRestHandler->getSubjectOnlineExams($obj['classId'],$obj['studentId'],$obj['subjectId'],$obj['date']);
             break;
 
 

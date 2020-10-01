61897e8f6780573332197f9df23d48106e2defe5
islamblmrbt <belmerabetislam@gmail.com>
2020-10-01 19:46:03 +0100

    reset test

--- app/api/StudentInfoController.php
+++ app/api/StudentInfoController.php
@@ -1,30 +1,11 @@
 <?php
 require_once("UserInfoRestHandler.php");
-require_once("UserInfo.php");		
-
-
-/*{
-	$view = "";
-if(isset($_GET["view"]) ){
-	$view = $_GET["view"];
-    }}
-    {
-        $studentId = "";
-    if(isset($_GET["studentId"]) ){
-        $studentId = $_GET["studentId"];
-        }}*/
-    
+require_once("UserInfo.php");		 
 $json = file_get_contents('php://input');
- 
-// decoding the received JSON and store into $obj variable.
 $obj = json_decode($json,true);
-
-// name store into $name.
 $view = $obj['view'];
 $studentId=$obj['studentId'];
     switch($view){
-
-  
             case "class":
                 $userinfoRestHandler = new UserInfoRestHandler();
                 $userinfoRestHandler->getStudentClass($studentId);
@@ -34,5 +15,4 @@ $studentId=$obj['studentId'];
                     $userinfoRestHandler->getStudentParents($studentId);
                 break;
 }
-    
     ?>
